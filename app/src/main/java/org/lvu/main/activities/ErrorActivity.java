ef76b5fdc046c96419899ffcad5158260b6f5d00
wuyr <2309474763@qq.com>
2016-07-21 01:43:56 +0800

    optimize code and fix some bugs

--- app/src/main/java/org/lvu/main/activities/ErrorActivity.java
+++ app/src/main/java/org/lvu/main/activities/ErrorActivity.java
@@ -155,7 +155,8 @@ public class ErrorActivity extends BaseActivity {
 
     private void changeToLandscape() {
         mTopView.setVisibility(View.GONE);
-        mBottomView.setVisibility(View.GONE);
+        if (mBottomView != null)
+            mBottomView.setVisibility(View.GONE);
         int uiFlags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -167,7 +168,8 @@ public class ErrorActivity extends BaseActivity {
 
     private void changeToPortrait() {
         mTopView.setVisibility(View.VISIBLE);
-        mBottomView.setVisibility(View.VISIBLE);
+        if (mBottomView != null)
+            mBottomView.setVisibility(View.VISIBLE);
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
